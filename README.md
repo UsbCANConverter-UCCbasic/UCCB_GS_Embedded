c976a45c6b574c6a67e6cb87062c39b3b00ed322
LJ <juranek.lukasz@gmail.com>
2021-06-07 20:21:22 +0200

    GS_UCCB_fix_queue

--- README.md
+++ README.md
@@ -1,8 +1,85 @@
-# UCCB_GS_Embedded
-Software for USBCanConverterBasic(UCCB) writen for STM32F042C6 mcu, see [ucandevices](https://ucandevices.github.io/uccb.html) web page for full info.
-# Build and run
-Source code is pure C. Project can be imported direclty to free vesrion of Atollic TrueSTUDIO for ARM 9 or higher 
-Project is based on [candleLight_gsusb](https://github.com/nexulm/candleLight_fw) 
-# Hardware
-Hardware is open source. See [CCBPCB](https://github.com/UsbCANConverter-UCCbasic/UCCBPCB) project for souces.
+# candleLight_gsusb
+[![Build Status](https://travis-ci.org/candle-usb/candleLight_fw.svg?branch=master)](https://travis-ci.org/candle-usb/candleLight_fw)
 
+This is firmware for certain STM32F042x/STM32F072xB-based USB-CAN adapters, notably:
+- candleLight: https://github.com/HubertD/candleLight (STM32F072xB)
+- cantact: http://linklayer.github.io/cantact/ (STM32F042C6)
+- canable (cantact clone): http://canable.io/ (STM32F042C6)
+- USB2CAN: https://github.com/roboterclubaachen/usb2can (STM32F042x6)
+- CANAlyze: https://kkuchera.github.io/canalyze/ (STM32F042C6)
+- VulCAN Gen1: https://shop.copperforge.cc/products/ac41 (STM32F042x6)
+- Entreé: https://github.com/tuna-f1sh/entree (STM32F042x6)
+
+Of important note is that the common STM32F103 will NOT work with this firmware because its hardware cannot use both USB and CAN simultaneously.
+Beware also the smaller packages in the F042 series which map a USB and CAN_TX signal on the same pin and are therefore unusable !
+
+This implements the interface of the mainline linux gs_usb kernel module and
+works out-of-the-box with linux distros packaging this module, e.g. Ubuntu.
+
+## Known issues
+
+Be aware that there is a bug in the gs_usb module in linux<4.5 that can crash the kernel on device removal.
+
+Here is a fixed version that should also work for older kernels:
+  https://github.com/HubertD/socketcan_gs_usb
+
+The Firmware also implements WCID USB descriptors and thus can be used on recent Windows versions without installing a driver.
+
+## Building
+
+Building requires arm-none-eabi-gcc toolchain.
+
+```shell
+sudo apt-get install gcc-arm-none-eabi
+
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/gcc-arm-none-eabi-8-2019-q3-update.cmake
+
+# or,
+# cmake-gui ..
+# don't forget to specify the cmake toolchain file before configuring.
+#
+# compile all targets :
+
+make
+
+# OR, each board target is a cmake option and can be disabled before running 'make';
+# OR, compile a single target , e.g.
+make cantact_fw
+
+#
+# to list possible targets :
+make help
+
+```
+
+## Flashing
+
+Flashing candleLight on linux: (source: [https://wiki.linklayer.com/index.php/CandleLightFirmware](https://wiki.linklayer.com/index.php/CandleLightFirmware))
+- Flashing requires the dfu-util tool. On Ubuntu, this can be installed with `sudo apt install dfu-util`.
+- compile as above, or download the current binary release: gsusb_cantact_8b2b2b4.bin
+- If dfu-util fails due to permission issues on Linux, you may need additional udev rules. Consult your distro's documentation and see `70-candle-usb.rules` provided here.
+
+### recommended simple method
+- If compiling with cmake, `make flash-<targetname_fw>`, e.g. `make flash-canable_fw`, to invoke dfu-util.
+
+### method for reflashing a specific device by serial
+- when multiple devices are connected, dfu-util may be unable to choose which one to flash.
+- Obtain device's serial # by looking at `dfu-util -l`
+- adapt the following command accordingly :
+ `dfu-util -D CORRECT_FIRMWARE.bin -S "serial_number_here", -a 0 -s 0x08000000:leave`
+- note, the `:leave` suffix above may not be supported by older builds of dfu-util and is simply a convenient way to reboot into the normal firmware.
+
+### fail-safe method (or if flashing a blank device)
+- Disconnect the USB connector from the CANtact, short the BOOT pins, then reconnect the USB connector. The device should enumerate as "STM32 BOOTLOADER".
+
+- invoke dfu-util manually with: `sudo dfu-util --dfuse-address -d 0483:df11 -c 1 -i 0 -a 0 -s 0x08000000 -D CORRECT_FIRWARE.bin` where CORRECT_FIRWARE is the name of the desired .bin.
+- Disconnect the USB connector, un-short the BOOT pins, and reconnect.
+
+
+
+
+## Links to related projects
+* [Cangaroo](https://github.com/HubertD/cangaroo) open source can bus analyzer software
+* [Candle.NET](https://github.com/elliotwoods/Candle.NET) .NET wrapper for the candle API
---
+++ include/config.h
@@ -0,0 +1,176 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Hubert Denkmair
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+#pragma once
+
+#define CAN_QUEUE_SIZE 64
+
+#define USBD_VID                     0x1d50
+#define USBD_PID_FS                  0x606f
+#define USBD_LANGID_STRING           1033
+#define USBD_CONFIGURATION_STRING_FS (uint8_t*) "gs_usb config"
+#define USBD_INTERFACE_STRING_FS     (uint8_t*) "gs_usb interface"
+
+#define BOARD_candleLight 1
+#define BOARD_cantact     2
+#define BOARD_canable     3
+#define BOARD_usb2can     4
+#define BOARD_canalyze     5
+#define BOARD_cannette    6
+#define BOARD_UCCB     7
+
+#define BOARD BOARD_UCCB
+
+#if BOARD == BOARD_candleLight
+	#define USBD_PRODUCT_STRING_FS		(uint8_t*) "candleLight USB to CAN adapter"
+	#define USBD_MANUFACTURER_STRING	(uint8_t*) "bytewerk"
+	#define DFU_INTERFACE_STRING_FS		(uint8_t*) "candleLight firmware upgrade interface"
+	#define CAN_S_Pin GPIO_PIN_13
+	#define CAN_S_GPIO_Port GPIOC
+
+	#define LED1_Pin GPIO_PIN_0
+	#define LED1_Mode GPIO_MODE_OUTPUT_OD
+	#define LED1_GPIO_Port GPIOA
+	#define LED1_Active_High 0
+
+	#define LED2_GPIO_Port GPIOA
+	#define LED2_Pin GPIO_PIN_1
+	#define LED2_Mode GPIO_MODE_OUTPUT_OD
+	#define LED2_Active_High 0
+
+#elif BOARD == BOARD_cantact
+	#define USBD_PRODUCT_STRING_FS		(uint8_t*) "cantact gs_usb"
+	#define USBD_MANUFACTURER_STRING	(uint8_t*) "cantact.io"
+	#define DFU_INTERFACE_STRING_FS		(uint8_t*) "cantact firmware upgrade interface"
+
+	// SILENT pin not connected
+
+	#define LED1_GPIO_Port GPIOB
+	#define LED1_Pin GPIO_PIN_0	/* green */
+	#define LED1_Mode GPIO_MODE_OUTPUT_PP
+	#define LED1_Active_High 1
+
+	#define LED2_GPIO_Port GPIOB
+	#define LED2_Pin GPIO_PIN_1	/* red */
+	#define LED2_Mode GPIO_MODE_OUTPUT_PP
+	#define LED2_Active_High 1
+
+#elif BOARD == BOARD_canable
+	#define USBD_PRODUCT_STRING_FS			(uint8_t*) "canable gs_usb"
+	#define USBD_MANUFACTURER_STRING		(uint8_t*) "canable.io"
+	#define DFU_INTERFACE_STRING_FS			(uint8_t*) "canable firmware upgrade interface"
+
+	// SILENT pin not connected
+
+	#define LED1_GPIO_Port GPIOB
+	#define LED1_Pin GPIO_PIN_0	/* green */
+	#define LED1_Mode GPIO_MODE_OUTPUT_PP
+	#define LED1_Active_High 1
+
+	#define LED2_GPIO_Port GPIOB
+	#define LED2_Pin GPIO_PIN_1	/* blue */
+	#define LED2_Mode GPIO_MODE_OUTPUT_PP
+	#define LED2_Active_High 0
+
+#elif BOARD == BOARD_usb2can
+	#define USBD_PRODUCT_STRING_FS		(uint8_t*) "USB2CAN RCA gs_usb"
+	#define USBD_MANUFACTURER_STRING	(uint8_t*) "Roboter Club Aachen"
+	#define DFU_INTERFACE_STRING_FS		(uint8_t*) "usb2can firmware upgrade interface"
+
+	// SILENT pin not connected
+
+	#define LED4_GPIO_Port GPIOA
+	#define LED4_Pin GPIO_PIN_0	/* white */
+	#define LED4_Mode GPIO_MODE_OUTPUT_OD
+	#define LED4_Active_Low
+
+	#define LED2_GPIO_Port GPIOA
+	#define LED2_Pin GPIO_PIN_1	/* blue */
+	#define LED2_Mode GPIO_MODE_OUTPUT_OD
+	#define LED2_Active_High 0
+
+	#define LED3_GPIO_Port GPIOA
+	#define LED3_Pin GPIO_PIN_2	/* red */
+	#define LED3_Mode GPIO_MODE_OUTPUT_OD
+	#define LED3_Active_Low
+
+	#define LED1_GPIO_Port GPIOB
+	#define LED1_Pin GPIO_PIN_3	/* green */
+	#define LED1_Mode GPIO_MODE_OUTPUT_OD
+	#define LED1_Active_High 0
+#elif BOARD == BOARD_canalyze
+	#define USBD_PRODUCT_STRING_FS		(uint8_t*) "CANAlyze gs_usb"
+	#define USBD_MANUFACTURER_STRING	(uint8_t*) "STMicroelectronics"
+	#define DFU_INTERFACE_STRING_FS		(uint8_t*) "CANAlyze firmware upgrade interface"
+
+	// SILENT pin not connected
+
+	#define LED1_GPIO_Port GPIOB
+	#define LED1_Pin GPIO_PIN_0	/* green */
+	#define LED1_Mode GPIO_MODE_OUTPUT_PP
+	#define LED1_Active_High 1
+
+	#define LED2_GPIO_Port GPIOB
+	#define LED2_Pin GPIO_PIN_1	/* red */
+	#define LED2_Mode GPIO_MODE_OUTPUT_PP
+	#define LED2_Active_High 1
+
+#elif BOARD == BOARD_cannette
+	#define USBD_PRODUCT_STRING_FS			(uint8_t*) "cannette gs_usb"
+	#define USBD_MANUFACTURER_STRING		(uint8_t*) "chacaltech"
+	#define DFU_INTERFACE_STRING_FS			(uint8_t*) "cannette firmware upgrade interface"
+
+	// SILENT pin not connected
+
+	#define LED1_GPIO_Port GPIOA
+	#define LED1_Pin GPIO_PIN_9	/* RX: green */
+	#define LED1_Mode GPIO_MODE_OUTPUT_OD
+	#define LED1_Active_High 0
+
+	#define LED2_GPIO_Port GPIOA
+	#define LED2_Pin GPIO_PIN_8	/* TX: red */
+	#define LED2_Mode GPIO_MODE_OUTPUT_OD
+	#define LED2_Active_High 0
+
+	#define nCANSTBY_Port GPIOC
+	#define nCANSTBY_Pin GPIO_PIN_14	/* control xceiver standby, active low */
+
+	#define nSI86EN_Port GPIOC
+	#define nSI86EN_Pin GPIO_PIN_13		/* enable power to Si86xx isolater, active low */
+
+	#define DCDCEN_Port GPIOC
+	#define DCDCEN_Pin GPIO_PIN_15		/* activate DCDC converter, active high */
+#elif BOARD == BOARD_UCCB
+	#define USBD_PRODUCT_STRING_FS		(uint8_t*) "UCCB USB to CAN adapter"
+	#define USBD_MANUFACTURER_STRING	(uint8_t*) "ucandevices.github.io"
+	#define DFU_INTERFACE_STRING_FS		(uint8_t*) "UCCB firmware upgrade interface"
+
+	#warning "UCCB Compilation"
+
+#else
+
+	#error please define BOARD
+#endif
